8c9f0bd0e04d51adb200a871e676f5c567bffddf
Joel Pires <joelpires70@gmail.com>
2023-10-14 18:09:27 -0300

    security tests

--- src/main/java/api/exception/NotAllowedException.java
+++
@@ -1,9 +0,0 @@
-package api.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class NotAllowedException extends ControllerException {
-    public NotAllowedException(String message) {
-        super(HttpStatus.FORBIDDEN, message);
-    }
-}
---
+++ src/test/java/api/security/JwtTokenFilterTests.java
@@ -0,0 +1,14 @@
+package api.security;
+
+import org.junit.jupiter.api.Test;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class JwtTokenFilterTests {
+
+
+    @Test
+    public void test() {
+        
+    }
+}
